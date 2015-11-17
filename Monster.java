69fdb681e69528030a943b62e300180fd2a5263f
BursTheGravity <yauyeung@stuy.edu>
2015-11-16 21:05:11 -0500

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,10 @@
+// Team Stuyrim - Leo Auyeung, Sungbin Kim, Jason Mohabir
+// APCS pd10
+// HW31 -- Ye Olde Role Playing Game, Unchained
+// 2015-11-16
+
+public class Monster extends Character {
+    public Monster(){
+		super(150,( (int)(Math.random() * 25 ) + 20),20,2.5);
+	}
+}
